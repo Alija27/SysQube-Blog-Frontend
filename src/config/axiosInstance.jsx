f31e3eaa57bf16dfa4eaf65b73a38a5300851a0b
Alija Ghimire <alijaghimire27@gmail.com>
2024-06-07 19:27:16 +0545

    [feat] updated axiosInstance

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -1,12 +1,25 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        "Authorization": "Bearer " + localStorage.getItem("token"),
         "Content-Type": "application/json",
         "timeout": 1000,
     },
 });
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers["Authorization"] = "Bearer " + token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 
 export default axiosInstance;
